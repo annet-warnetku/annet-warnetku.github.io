7c0b84e1d3b7f9bf979061e37576478e566b52e7
annet-warnetku <58595139+annet-warnetku@users.noreply.github.com>
2020-06-28 12:02:12 +0700

    Add files via upload

---
+++ wew.js
@@ -0,0 +1,12 @@
+$("#accordion").on("hidden.bs.collapse", function (e) {
+        $(e.target).closest(".panel-primary")
+            .find(".panel-heading span")
+            .removeClass("glyphicon glyphicon-minus")
+            .addClass("glyphicon glyphicon-plus");
+    });
+    $("#accordion").on("shown.bs.collapse", function (e) {
+        $(e.target).closest(".panel-primary")
+            .find(".panel-heading span")
+            .removeClass("glyphicon glyphicon-plus")
+            .addClass("glyphicon glyphicon-minus");
+    });
